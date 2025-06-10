1d37614f0c834264ae75927e679a8486c5f020fe
Tanisha <tanishapurushottamlande@gmail.com>
2025-06-10 15:18:55 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -75,10 +75,10 @@ if remaining >= 10:
         remaining_str = str(remaining)
         first_digit = int(remaining_str[0])
         second_digit = int(remaining_str[1]) 
-percentage = int(str(matching * 2 + first_digit) + second_digit)
+        percentage = int(str(matching * 2 + first_digit) + second_digit)
 else:
         # If remaining is less than 10, use original logic
-percentage = int(str(matching * 2) + str(remaining)) if matching * 2 + remaining > 0 else 0
+        percentage = int(str(matching * 2) + str(remaining)) if matching * 2 + remaining > 0 else 0
     
     # Ensure percentage doesn't exceed 100%
     if percentage > 100:
