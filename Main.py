ce28ba65d96e0d6ae0e7704190086055cd3b2616
Tanisha <tanishapurushottamlande@gmail.com>
2025-06-10 15:35:38 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -85,7 +85,7 @@ if percentage > 100:
         percentage = 100
 
     # Dynamic title
-    title = f"bond between {name1} and your {relation.capitalize()}💖:"
+title = f"bond between {name1} and your {relation.capitalize()}💖:"
 
     # Dynamic message
     if relation.lower() == "father":
